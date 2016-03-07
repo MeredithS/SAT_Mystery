4a3340402132796e68dd159723294778b68d0a1f
Meredith Salguero <salguero.meredith@gmail.com>
2016-03-07 15:51:46 -0500

    save migration

--- db/migrate/20160307200810_create_scores.rb
+++ db/migrate/20160307200810_create_scores.rb
@@ -1,6 +1,10 @@
 class CreateScores < ActiveRecord::Migration
   def change
     create_table :scores do |t|
+    	t.boolean :hint_used
+    	t.boolean :answer_correct
+    	t.references :user
+    	t.references :question
     end
   end
 end
